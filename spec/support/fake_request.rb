f50f311d63348dcddf44b63c2a9fde65a58b74d3
joconnor <jonathan.oconnor@xcom.de>
2015-05-19 12:01:00 +0200

    I removed the use of named arguments, and changed the initializer back to using an options hash, with default values for the hash values.

--- spec/support/fake_request.rb
+++ spec/support/fake_request.rb
@@ -1,13 +1,9 @@
 class FakeRequest
 
-  def initialize(
-    remote_ip: "23.12.54.111",
-    user_agent: "I am not a bot",
-    headers: {}
-  )
-    self.remote_ip = remote_ip
-    self.user_agent = user_agent
-    self.headers = headers
+  def initialize(options = {})
+    self.remote_ip = options.fetch(:remote_ip, "23.12.54.111")
+    self.user_agent = options.fetch(:user_agent, "I am not a bot")
+    self.headers = options.fetch(:headers, {})
   end
 
   attr_accessor :remote_ip
